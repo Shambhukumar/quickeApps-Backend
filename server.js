9db0faf0823d6dcad1e6b9cedd185f1dc03dd5d0
Shambhu Sahu <shambhukumar.shau@gmail.com>
2021-04-11 14:10:02 +0530

    updated the re-populated and added a crom job

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-const express = require("express")
-const app = express();
-const port = 4000;
-const cors = require("cors");
-require("dotenv").config();
-const StockData = require("./controllers/StockData");
-app.options("*",cors())
-app.use(express.json());
-app.use((req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept")
-  next();
-});
+  const express = require("express")
+  const app = express();
+  const port = 4000;
+  const cors = require("cors");
+  var cron = require("node-cron");
+  require("dotenv").config();
+  const StockData = require("./controllers/StockData");
+  app.options("*",cors())
+  app.use(express.json());
+  app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept")
+    next();
+  });
 
-const mongoose = require('mongoose');
-process.env.DATABASE
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
+  const mongoose = require('mongoose');
+  mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
- console.log("Connected To the DataBase")
-});
-app.get("/getdata", StockData.getallData)
-app.delete("/delete/:id", StockData.deleteOnce)
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+  console.log("Connected To the DataBase")
+  });
+  app.get("/getdata", StockData.getallData)
+  app.delete("/delete/:id", StockData.deleteOnce)
 
+  cron.schedule("*/1 * * * *", function() {
+    StockData.PushStockData()
+    console.log("running a task every 1 minutes");
+  });
 
-StockData.PushStockData();
-
-app.listen(process.env.PORT || port,()=>{
-  console.log(`Server is Running on PORT: ${port}`)
-})
+  app.listen(process.env.PORT || port,()=>{
+    console.log(`Server is Running on PORT: ${port}`)
+  })
